3ddbaae09e58795daae0e557c50640dff6e8689d
UbayGD <ubay.glezdiaz@gmail.com>
2021-08-05 19:39:34 +0100

    Add Router to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import AppBarComponent from './components/appbar.component/appbar.component';
+import HomeComponent from './components/home.component/home.component';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <AppBarComponent />
+        <Switch>
+          <Route exact path="/">
+            <HomeComponent />
+          </Route>
+          <Route path="/character/:id">
+            <></>
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
