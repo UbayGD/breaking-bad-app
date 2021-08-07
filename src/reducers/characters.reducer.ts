54ddedb85ba986cf88a01bd7f9a122afd7dbe5f6
UbayGD <ubay.glezdiaz@gmail.com>
2021-08-07 08:43:55 +0100

    Add class-transformer

--- src/reducers/characters.reducer.ts
+++ src/reducers/characters.reducer.ts
@@ -1,3 +1,4 @@
+import { plainToClass } from "class-transformer";
 import { AnyAction } from "redux";
 import { CHARACTERS_ACTIONS_CONSTANTS } from "../constants/actions/characters.constants";
 import { CharacterModel } from "../models/character.model";
@@ -7,7 +8,7 @@ const initialState: {characters?: CharacterModel[]} = {};
 export const charactersReducer = (state = initialState, action: AnyAction) => {
     switch (action.type) {
         case CHARACTERS_ACTIONS_CONSTANTS.GET_CHARACTERS_SUCCESS:
-            return {...state, characters: action.payload};
+            return {...state, characters: action.payload.map((character: object) => plainToClass(CharacterModel, character))};
         default:
             return state;
     }
