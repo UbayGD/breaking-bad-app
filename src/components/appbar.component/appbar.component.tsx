b865b27e63584a90491cce6dd0a2204cb742f670
UbayGD <ubay.glezdiaz@gmail.com>
2021-08-09 16:29:30 +0100

    Add back button to AppBar

--- src/components/appbar.component/appbar.component.tsx
+++ src/components/appbar.component/appbar.component.tsx
@@ -1,14 +1,20 @@
+import { useHistory, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { useStyles } from './appbar.styles';
-import { FormControl, Select, MenuItem } from '@material-ui/core';
+import { FormControl, Select, MenuItem, IconButton } from '@material-ui/core';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import Flag from 'react-world-flags';
 
 const AppBarComponent = () => {
-    const { i18n } = useTranslation()
+    const { i18n } = useTranslation();
+    let history = useHistory();
+    const location = useLocation();
     const classes = useStyles();
+
+    const isHome = (): boolean => location.pathname === '/';
 
     const handleChangeLanguage = (event: React.ChangeEvent<{ value: unknown }>) => {
         i18n.changeLanguage(event.target.value as string);
@@ -32,10 +38,23 @@ const AppBarComponent = () => {
         );
     }
 
+    const handleBackButton = () => {
+        history.push('/')
+    }
+
+    const renderBackButton = () => {
+        return (
+            <IconButton aria-label="back" onClick={handleBackButton}>
+                <ArrowBackIcon />
+            </IconButton>
+        )
+    }
+
     return (
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
+                    {!isHome() && renderBackButton()}
                     <Typography className={classes.title} variant="h6" noWrap>
                         Breaking Bad App
                     </Typography>
